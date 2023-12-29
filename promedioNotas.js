216ca5005488cc81a1727271127189edf94daaf7
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-28 21:28:33 -0500

    Cambios Realizados

--- promedioNotas.js
+++ promedioNotas.js
@@ -5,9 +5,17 @@ calcularPromedioNotas = function(){
     let promedio;
     promedio = calcularPromedio(valorNota1,valorNota2,valorNota3).toFixed(2);
     cambiarTexto("lblPromedio", promedio);
-    if(promedio >=7){
-        cambiarImagen("imgNotas","./images/bien.gif");
+    if(promedio < 5 && promedio > 0){
+        cambiarImagen("imgNotas","./images/bad.gif");
+        cambiarTexto("lblMensaje", "REPROBADO");
+    } else if(promedio >= 5 && promedio <=8){
+        cambiarImagen("imgNotas", "./images/bien2.gif");
+        cambiarTexto("lblMensaje", "BUEN TRABAJO");
+    } else if(promedio > 8 && promedio <= 10){
+        cambiarImagen("imgNotas", "./images/bien.gif")
+        cambiarTexto("lblMensaje", "EXCELENTE");
     } else {
-        cambiarImagen("imgNotas", "./images/bad.gif");
+        cambiarImagen("imgNotas", "./images/error.gif");
+        cambiarTexto("lblMensaje", "DATOS INCORRECTOS");
     }
 };
